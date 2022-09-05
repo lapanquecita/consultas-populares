a12d4bf22d8cb187b51df19e3e378a84bcf2afd2
Montse <79296842+lapanquecita@users.noreply.github.com>
2022-09-04 19:17:21 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# consultas-populares
+# Consultas populares en México
+
+Hasta la fecha de escribir este artículo el INE ha organizado dos consultas populares en México.
+
+La primera, en el año 2021 tenía comoo propósito el iniciar una investigación judicial para determinar si los actores políticos del pasado habián cometido crímenes.
+
+La segunda, en el año 2022 tenía como propósito la terminación de funciones del titular del poder Ejecutivo.
+
+Ambas consultas necesitan un mínimo del 40% de participación a nivel nacional para ser vinculantes. En ambos casos la participación fue mucho menor a la requerida.
+
+En este repositorio vamos a analizar los datos proporcionados por el INE.
+
+## Consulta Popular 2021
+
+![Imagen 1](./2021.png)
+
+## Consulta Popular 2022
+
+
+![Imagen 2](./2022.png)
---
+++ consulta2021.py
@@ -0,0 +1,138 @@
+import json
+
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+
+
+def main():
+
+    # Cargamos nuestro archivo JSON.
+    data = json.load(open("./data/2021.json", "r", encoding="utf-8"))
+
+    # Obtenemos el total nacional.
+    total_nacional = data["totalVotos"]
+    participacion_nacional = data["porcentajeParticipacionCiudadana"]
+
+    subtitulo = f"Nacional: {participacion_nacional:,.2f}% ({total_nacional:,.0f} votos)"
+
+    entidades = dict()
+
+    # Iteramos sobre todas las entidades.
+    for entidad in data["entidadesHijas"][:-1]:
+
+        # Limpiamos el nombre de la entidad.
+        nombre = entidad["nombreNodo"].title().replace("De", "de")
+
+        if nombre == "México":
+            nombre = "Estado de México"
+
+        # Extraemos el valor que nos interesa.
+        participacion = entidad["porcentajeParticipacionCiudadana"]
+
+        entidades[nombre] = [participacion]
+
+    # Creamos un DataFrame con los valores de nuestro diccionario.
+    df = pd.DataFrame.from_dict(
+        entidades, orient="index", columns=["participacion"])
+
+    ubicaciones = list()
+    valores = list()
+
+    marcas = np.arange(3, 11.5 + 0.5, 0.85)
+    etiquetas = list()
+
+    for marca in marcas:
+        etiquetas.append(f"{marca:,.1f}%")
+
+    geojson = json.load(open("./mexico.json",  "r", encoding="utf-8"))
+
+    # Iteramos sobre las entidades dentro del GeoJSON.
+    for item in geojson["features"]:
+        geo = item["properties"]["ADMIN_NAME"]
+        ubicaciones.append(geo)
+        valores.append(df.loc[geo, "participacion"])
+
+    fig = go.Figure()
+
+    fig.add_traces(
+        go.Choropleth(
+            geojson=geojson,
+            locations=ubicaciones,
+            z=valores,
+            featureidkey="properties.ADMIN_NAME",
+            colorscale="portland",
+            colorbar={"x": 0.03, "y": 0.5, "ypad": 50, "ticks": "outside", "outlinewidth": 2,
+                      "outlinecolor": "#FFFFFF", "tickvals": marcas, "ticktext": etiquetas,
+                      "tickwidth": 3, "tickcolor": "#FFFFFF", "ticklen": 10, "tickfont_size": 20},
+            marker_line_color="white",
+            marker_line_width=1.25,
+            zmin=3.0, zmax=11.5
+        )
+    )
+
+    fig.update_geos(
+        fitbounds="geojson", showocean=True, oceancolor="#082032",
+        showcountries=False, framecolor="#FFFFFF", framewidth=2,
+        showlakes=False, coastlinewidth=0, landcolor="#1C0A00"
+    )
+
+    fig.update_layout(
+        showlegend=False,
+        font_family="Quicksand",
+        font_color="#FFFFFF",
+        margin={"r": 40, "t": 50, "l": 40, "b": 30},
+        width=1280,
+        height=720,
+        paper_bgcolor="#334756",
+        annotations=[
+            dict(
+                x=0.0275,
+                y=0.45,
+                textangle=-90,
+                xanchor="center",
+                yanchor="middle",
+                text="Proporción relativa al padrón electoral por entidad",
+                font_size=16
+            ),
+            dict(
+                x=0.5,
+                y=1.0,
+                xanchor="center",
+                yanchor="top",
+                text="Distribución por entidad del porcentaje de participación en la consulta popular del año 2021 en México",
+                font_size=24
+            ),
+            dict(
+                x=0.01,
+                y=-0.03,
+                xanchor="left",
+                yanchor="top",
+                text="Fuente: INE (2021)",
+                font_size=22
+            ),
+            dict(
+                x=0.5,
+                y=-0.03,
+                xanchor="center",
+                yanchor="top",
+                text=subtitulo,
+                font_size=22
+            ),
+            dict(
+                x=1.01,
+                y=-0.03,
+                xanchor="right",
+                yanchor="top",
+                text="🧁 @lapanquecita",
+                font_size=22
+            )
+        ]
+    )
+
+    fig.write_image("./2021.png")
+
+
+if __name__ == "__main__":
+
+    main()
